5b502b45d98f3953572cb2ee1161d431b8cc1105
1Slivador1Dali1 <vladislav.lashkov@gmail.com>
2025-11-19 02:54:39 +0300

    feat: reading and writing files

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,36 @@
 import { createServer } from "node:http";
+import fs from "node:fs";
+import path from "node:path";
 
 const port = 3000;
 
 const server = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-type", "text/plain");
-  res.end("Hello World");
+
+  res.end("test");
 });
 
 server.listen(port, () => {
   console.log(`http://localhost:${port}`);
 });
+
+const content = "test Content";
+
+fs.appendFile(
+  "/home/lashkovvladislav/Projects/SMUS-Backend/src/test.txt",
+  content,
+  (err) => {}
+);
+
+fs.readFile(
+  "/home/lashkovvladislav/Projects/SMUS-Backend/src/test.txt",
+  "utf8",
+  (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(data);
+  }
+);
