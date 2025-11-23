d7a8a4f855d0a96a94ae404b583607f876cfb5ff
1Slivador1Dali1 <vladislav.lashkov64@gmail.com>
2025-11-23 15:08:25 +0300

    feat: notions read\create\delete endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,70 @@
-import { createServer } from "node:http";
-import fs from "node:fs";
-import path from "node:path";
+import express from "express";
+import { notions } from "./mockDB.ts";
 
+const app = express();
 const port = 3000;
 
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-type", "text/plain");
+app.use(express.json());
 
-  res.end("test");
+app.get("/", (req, res) => {
+  res.send("Home Express!");
 });
 
-server.listen(port, () => {
+app.get("/notions", (req, res) => {
+  res.status(200).json(notions);
+});
+
+app.get("/notions/:id", (req, res) => {
+  const notionId = req.params.id;
+  const notion = notions.items.find((n) => n.id === notionId);
+
+  if (!notion) {
+    res.status(404).json({ error: "Notion not found" });
+    return;
+  }
+
+  res.status(200).json(notion);
+});
+
+app.post("/notions", (req, res) => {
+  const { name, description } = req.body;
+
+  if (!name || !description) {
+    res.status(400).json({
+      error: "Name and description are required",
+    });
+    return;
+  }
+
+  const newNotion = {
+    id: String(notions.items.length + 1),
+    name,
+    description,
+  };
+
+  notions.items.push(newNotion);
+  res.status(201).json(newNotion);
+});
+
+app.delete("/notions/:id", (req, res) => {
+  const notionId = req.params.id;
+  const index = notions.items.findIndex((n) => n.id === notionId);
+
+  if (!notionId) {
+    res.status(400).send("Bad Request");
+    return;
+  }
+
+  if (index === -1) {
+    res.status(404).json({ error: "Notion not found" });
+    return;
+  }
+
+  notions.items.splice(index, 1);
+
+  res.status(204).send();
+});
+
+app.listen(port, () => {
   console.log(`http://localhost:${port}`);
 });
-
-const content = "test Content";
-
-fs.appendFile(
-  "/home/lashkovvladislav/Projects/SMUS-Backend/src/test.txt",
-  content,
-  (err) => {}
-);
-
-fs.readFile(
-  "/home/lashkovvladislav/Projects/SMUS-Backend/src/test.txt",
-  "utf8",
-  (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(data);
-  }
-);
---
+++ src/mockDB.ts
@@ -0,0 +1,16 @@
+export let notions = {
+  items: [
+    {
+      id: "1",
+      name: "first notion",
+      description: "First notion description",
+    },
+    {
+      id: "2",
+      name: "second notion",
+      description: "Second notion description",
+    },
+  ],
+  offset: null,
+  page: 1,
+};
